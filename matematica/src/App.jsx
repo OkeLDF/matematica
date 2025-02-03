90324603631c04d91b8b9ea267895c7b33fc5b93
OkeLDF <otaviofolador43@gmail.com>
2025-02-03 15:49:48 -0300

    Update App.jsx

--- matematica/src/App.jsx
+++ matematica/src/App.jsx
@@ -1,25 +1,7 @@
-import { JSXGraph } from "jsxgraph"
-
 function App() {
-  const board = JSXGraph.initBoard(
-    'box',
-    {
-      boundingbox: []
-    }
-  )
-
   return (
     <>
       <h1>Hello World</h1>
-      <div
-        id="box"
-        className="jxgbox border "
-        style={{
-          width: '600px',
-          height: '600px',
-          backgroundColor: 'white'
-        }}
-      />
     </>
   )
 }
